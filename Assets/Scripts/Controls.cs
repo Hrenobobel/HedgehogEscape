3888368d17db53a1fa6e248172731c29a69c1c50
Hrenobobel <elavek@mail.ru>
2022-02-05 17:52:56 +0300

    Realisation of smooth rotation/move
    Smooth move realised in coroutines

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -8,16 +8,18 @@ public class Controls : MonoBehaviour
     public float MaxSwipe;
     //Логика группы ежей
     public Player player;
+    //Скорость поворота
+    public float MoveSpeed;
 
     //Предыдущая позиция игрока
     private Vector3 LastPosition;
     private Quaternion LastRotation;
-
     //Отработка реакции на нажатие мыши
     private Vector3 _previousMousePosition;
-
+    //Указатель на столкновение со стеной
+    private bool WallTrigger;
+    //Корутины плавного поворота
     Coroutine coroutine;
-    private Transform end;
 
     void Update()
     {
@@ -50,85 +52,149 @@ public class Controls : MonoBehaviour
 
         //Управление с клавиатуры
         if (Input.GetKeyUp(KeyCode.W))
-            if (coroutine == null)
-            {
-                coroutine = StartCoroutine(Rotate(90.0f, 0.5f));
-            }
-        //МoveUp();
+            if (coroutine == null)      //Выполняем движение если другого не производится
+                coroutine = StartCoroutine(RotateUp());                
 
         if (Input.GetKeyUp(KeyCode.S))
-            МoveDown();
+            if (coroutine == null)      //Выполняем движение если другого не производится
+                coroutine = StartCoroutine(RotateDown());
 
         if (Input.GetKeyUp(KeyCode.D))
-            МoveRight();
+            if (coroutine == null)      //Выполняем движение если другого не производится
+                coroutine = StartCoroutine(RotateRight());
 
         if (Input.GetKeyUp(KeyCode.A))
-            МoveLeft();
+            if (coroutine == null)      //Выполняем движение если другого не производится
+                coroutine = StartCoroutine(RotateLeft());
     }
-    public void МoveUp ()   //Объект игрока двигается вперёд по оси Z
-    {
-        SavePlayerPosition();
-        Vector3 up = player.GetUpCorner();
-        //Поворот ежей относительно требуемой оси
-        
-        player.transform.RotateAround(up, Vector3.right, 90f);
-    }
-    public void МoveDown()  //Объект игрока двигается назад по оси Z
-    {
-        SavePlayerPosition();
-        Vector3 down = player.GetDownCorner();
-        //Поворот ежей относительно требуемой оси
-        player.transform.RotateAround(down, Vector3.left, 90f);
-    }
-    public void МoveRight() //Объект игрока двигается вправо по оси X
-    {
-        SavePlayerPosition();
-        Vector3 right = player.GetRightCorner();
-        //Поворот ежей относительно требуемой оси
-        player.transform.RotateAround(right, Vector3.back, 90f);
-    }
-    public void МoveLeft()  //Объект игрока двигается влево по оси X
-    {
-        SavePlayerPosition();
-        Vector3 left = player.GetLeftCorner();
-        //Поворот ежей относительно требуемой оси
-        player.transform.RotateAround(left, Vector3.forward, 90f);
-    }
-    private void SavePlayerPosition()   //Сохранение текущего положения объекта игрока
+    private void SavePlayerPosition()   //Сохранение текущей позиции объекта игрока
     {
         LastPosition = player.transform.position;
         LastRotation = player.transform.rotation;
+        WallTrigger = false;            //Сбрасываем указатель столкновения со стеной
     }
-    private void SetPlayerPosition()   //Установка положения объекта игрока
+    private void SetPlayerPosition()    //Установка объекта игрока в передыдущую позицию
     {
         player.transform.position = LastPosition;
         player.transform.rotation = LastRotation;
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)     //при касании стены срабатывает возвращающий игрока на предыдущую позицию триггер
     {
-        if (other.gameObject.CompareTag("Wall"))    //На объекте стены установлен возвращающий игрока на предыдущую позицию триггер
+        if (other.gameObject.CompareTag("Wall"))    
         {
-            SetPlayerPosition();
+            WallTrigger = true;     //Выход из функции плавного поворота
+            SetPlayerPosition();    //Возвращем игрока в предыдущее положение
         }
     }
-
-    IEnumerator Rotate(float angle, float intensity)
+    public void МoveUp ()   //Позиция игрока поворачивается на 90 градусов вперёд по оси Z
     {
-        end = player.transform;
-        Vector3 up = player.GetUpCorner();
-        end.RotateAround(up, Vector3.right, angle);
-
-        while (true)
+        Vector3 up = player.GetUpCorner();                          //Берем переднюю нижнюю ось группы ежей для поворота
+        player.transform.RotateAround(up, Vector3.right, 90f);      //Поворот ежей вперёд
+    }
+    public void МoveDown()  //Позиция игрока поворачивается на 90 градусов назад по оси Z
+    {
+        Vector3 down = player.GetDownCorner();                      //Берем заднюю нижнюю ось группы ежей для поворота
+        player.transform.RotateAround(down, Vector3.left, 90f);     //Поворот ежей назад
+    }
+    public void МoveRight() //Позиция игрока поворачивается на 90 градусов вправо по оси X
+    {
+        Vector3 right = player.GetRightCorner();                    //Берем правую нижнюю ось группы ежей для поворота
+        player.transform.RotateAround(right, Vector3.back, 90f);    //Поворот ежей вправо
+    }
+    public void МoveLeft()  //Позиция игрока поворачивается на 90 градусов влево по оси X
+    {
+        Vector3 left = player.GetLeftCorner();                      //Берем левую нижнюю ось группы ежей для поворота
+        player.transform.RotateAround(left, Vector3.forward, 90f);  //Поворот ежей влево
+    }
+    IEnumerator RotateUp()      //Реализация плавного поворота игрока вперёд
+    {
+        SavePlayerPosition();               //Сохраняем стартовое положение игрока
+        Vector3 up = player.GetUpCorner();  //Берем переднюю нижнюю ось группы ежей для поворота
+        float rotation = 0f;                //Счетчик для контроля "доворота до 90 градусов"        
+        while (true) 
         {
-            player.transform.rotation = Quaternion.Lerp(player.transform.rotation, end.rotation, intensity * Time.deltaTime);
+            if (WallTrigger) yield break;   //Если столкнулись со стеной, выходим
 
-            if (Quaternion.Angle(player.transform.rotation, end.rotation) < 0.01f)
-            {
-                coroutine = null;
-                player.transform.rotation = end.rotation;
+            float angle = Mathf.LerpAngle(0f, 90f, MoveSpeed * Time.deltaTime);
+            player.transform.RotateAround(up, Vector3.right, angle);
+            rotation += angle;
+            
+            if (rotation > 90f - MoveSpeed) //Вращаем игрока пока угол поворота не приблизится к 90 градусам на угол менее шага, равного скорости
+            {                
+                SetPlayerPosition();        //Для установки игрока в точное положение возвращаем его в предыдущее состояние
+                МoveUp();                   // и проворачиваем точно на 90 градусов
                 yield break;
             }
+            coroutine = null;
+            yield return null;
+        }
+    }
+    IEnumerator RotateDown()    //Реализация плавного поворота игрока назад
+    {
+        SavePlayerPosition();                   //Сохраняем стартовое положение игрока
+        Vector3 down = player.GetDownCorner();  //Берем заднюю нижнюю ось группы ежей для поворота
+        float rotation = 0f;                    //Счетчик для контроля "доворота до 90 градусов"        
+        while (true)
+        {
+            if (WallTrigger) yield break;   //Если столкнулись со стеной, выходим
 
+            float angle = Mathf.LerpAngle(0f, 90f, MoveSpeed * Time.deltaTime);
+            player.transform.RotateAround(down, Vector3.left, angle);
+            rotation += angle;
+
+            if (rotation > 90f - MoveSpeed) //Вращаем игрока пока угол поворота не приблизится к 90 градусам на угол менее шага, равного скорости
+            {
+                SetPlayerPosition();        //Для установки игрока в точное положение возвращаем его в предыдущее состояние
+                МoveDown();                 // и проворачиваем точно на 90 градусов
+                yield break;
+            }
+            coroutine = null;
+            yield return null;
+        }
+    }
+    IEnumerator RotateRight()   //Реализация плавного поворота игрока вправо
+    {
+        SavePlayerPosition();                       //Сохраняем стартовое положение игрока
+        Vector3 right = player.GetRightCorner();    //Берем правую нижнюю ось группы ежей для поворота
+        float rotation = 0f;                        //Счетчик для контроля "доворота до 90 градусов"        
+        while (true)
+        {
+            if (WallTrigger) yield break;   //Если столкнулись со стеной, выходим
+
+            float angle = Mathf.LerpAngle(0f, 90f, MoveSpeed * Time.deltaTime);
+            player.transform.RotateAround(right, Vector3.back, angle);
+            rotation += angle;
+
+            if (rotation > 90f - MoveSpeed) //Вращаем игрока пока угол поворота не приблизится к 90 градусам на угол менее шага, равного скорости
+            {                
+                SetPlayerPosition();        //Для установки игрока в точное положение возвращаем его в предыдущее состояние
+                МoveRight();                // и проворачиваем точно на 90 градусов
+                yield break;
+            }
+            coroutine = null;
+            yield return null;
+        }
+    }
+    IEnumerator RotateLeft()    //Реализация плавного поворота игрока влево
+    {
+        SavePlayerPosition();                   //Сохраняем стартовое положение игрока
+        Vector3 left = player.GetLeftCorner();  //Берем левую нижнюю ось группы ежей для поворота
+        float rotation = 0f;                    //Счетчик для контроля "доворота до 90 градусов"        
+        while (true)
+        {
+            if (WallTrigger) yield break;   //Если столкнулись со стеной, выходим
+
+            float angle = Mathf.LerpAngle(0f, 90f, MoveSpeed * Time.deltaTime);
+            player.transform.RotateAround(left, Vector3.forward, angle);
+            rotation += angle;
+
+            if (rotation > 90f - MoveSpeed) //Вращаем игрока пока угол поворота не приблизится к 90 градусам на угол менее шага, равного скорости
+            {                
+                SetPlayerPosition();        //Для установки игрока в точное положение возвращаем его в предыдущее состояние
+                МoveLeft();                 // и проворачиваем точно на 90 градусов
+                yield break;
+            }
+            coroutine = null;
             yield return null;
         }
     }
